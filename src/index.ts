7bc72f5905971f3270e3fc087f661f802042b3a4
Ahra Cho <choar816@postech.ac.kr>
2022-07-07 20:56:46 +0900

    Dockerfile 수정

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import express, { Request, Response } from 'express';
 const app = express();
 
-app.get('/', (req, res) => {
+app.get('/', (req: Request, res: Response) => {
   const name = process.env.NAME || 'World';
   res.send(`Hello ${name}!`);
 });
