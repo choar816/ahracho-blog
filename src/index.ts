4caac744c061c3ba3b69fd4b5467e13e1de94739
Ahra Cho <choar816@postech.ac.kr>
2022-07-07 20:42:21 +0900

    typescript 적용

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-const express = require('express');
+import express, { Request, Response } from 'express';
 const app = express();
 
 app.get('/', (req, res) => {
@@ -6,7 +6,8 @@ app.get('/', (req, res) => {
   res.send(`Hello ${name}!`);
 });
 
-const port = parseInt(process.env.PORT) || 8080;
+const port = parseInt(`${process.env.PORT}`) || 8080;
+
 app.listen(port, () => {
   console.log(`helloworld: listening on port ${port}`);
 });
